3de38e17bb6ff4ee98c5b55f270a7070dd0465ff
ikramamu <mohdikram0410@gmail.com>
2023-02-27 12:14:57 -0800

    Midterm

--- models/trails.js
+++ models/trails.js
@@ -4,9 +4,19 @@ const Schema = mongoose.Schema;
 
 const TrailsSchema = new Schema({
     title: String,
+    image: String,
     description: String,
     location: String,
-    trailType: String
+    difficultyLevel: String,
+    trailLength: Number,
+    fitnessLevel: String,
+    ratings:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Rating'
+        }
+    ]
+
 });
 
 module.exports = mongoose.model('Trails', TrailsSchema);
